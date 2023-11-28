ed5fbe8ebab92218902ec969f07b44ab2df4e846
jadikanskywalker <awesomejaden.hicks@gmail.com>
2023-11-28 04:10:13 +0000

    new commit

--- .ipynb_checkpoints/git_push-checkpoint.sh
+++ .ipynb_checkpoints/git_push-checkpoint.sh
@@ -5,6 +5,6 @@ git add -f .
 git commit -m "New commit"
 
 git remote remove origin
-git remote add origin https://jadikanskywalker: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@github.com/jadikanskywalker/mariobros_v2.git
+git remote add origin https://jadikanskywalker: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@github.com/jadikanskywalker/mariobros_v2.git
 
 git push -u origin main
